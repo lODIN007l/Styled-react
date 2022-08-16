caeea4273047ca2dad2a8700d10f13a1df00fecb
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-16 03:28:31 -0500

    shape Intro

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import Intro from "./components/Intro";
 
 const Contenedor = styled.div`
   height: 100vh;
+  overflow: hidden;
   /* background-color: red; */
 `;
-
+const IntroShape = styled.div`
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  position: absolute;
+  z-index: -1;
+  clip-path: polygon(99% 0%, 61% 0%, 48% 100%, 99% 99%);
+  background-color: crimson;
+`;
 function App() {
   return (
     <Contenedor>
       <Navbar />
       <Intro />
+      <IntroShape />
     </Contenedor>
   );
 }
