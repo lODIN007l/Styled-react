f4536dade24e9bbffdcaf0a90041196f4ec9d428
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-16 02:47:53 -0500

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  height: 50px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+  text-decoration: underline crimson;
+`;
+const Menu = styled.ul`
+  list-style: none;
+  display: flex;
+`;
+const MenuItem = styled.li`
+  margin-right: 30px;
+  font-size: 20px;
+  font-weight: bold;
+  color: gray;
+`;
+const Boton = styled.button`
+  border: 2px solid white;
+  padding: 10px 15px;
+  background-color: crimson;
+  color: white;
+  font-weight: bold;
+  border-radius: 19px;
+  cursor: pointer;
+`;
+
+const Navbar = () => {
+  return (
+    <Contenedor>
+      <Wrapper>
+        <Left>
+          <Logo>Logo</Logo>
+          <Menu>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>Features</MenuItem>
+            <MenuItem>Services</MenuItem>
+            <MenuItem>Pricing</MenuItem>
+            <MenuItem>Contact</MenuItem>
+          </Menu>
+        </Left>
+        <Boton>JOIN TODAY</Boton>
+      </Wrapper>
+    </Contenedor>
+  );
+};
+
+export default Navbar;
