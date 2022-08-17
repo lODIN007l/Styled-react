a2d953ac07a616b2635b17c0384902e53ec50da9
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-17 01:49:30 -0500

    diseño finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,41 @@
+import styled from "@emotion/styled";
 import React from "react";
 
+const Contenedor = styled.div`
+  height: 10%;
+  background-color: #652ed3;
+  color: lightgray;
+`;
+const Wrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+`;
+const ListItem = styled.li`
+  margin-right: 20px;
+`;
+const Copyright = styled.span``;
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Contenedor>
+      <Wrapper>
+        <List>
+          <ListItem>Guide</ListItem>
+          <ListItem>Support</ListItem>
+          <ListItem>API</ListItem>
+          <ListItem>Comunnity</ListItem>
+        </List>
+        <Copyright>Wilson Lluilema -2022</Copyright>
+      </Wrapper>
+    </Contenedor>
+  );
 };
 
 export default Footer;
